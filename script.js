440875328499ec124bd38216cba783b156ad5c91
jonathandennis <4jondennis@gmail.com>
2020-05-17 15:11:12 +0200

    ES6 exercises

--- script.js
+++ script.js
@@ -95,9 +95,8 @@ for (var i = 0; i < 5; i++) {
 
 console.log(i); // 5
 */
-
+/*
 // blocks and IIFEs
-
 {
     const a = 1;
     let b = 2,
@@ -110,6 +109,128 @@ console.log(a + b); // error as variables not accessible outside of block. GREAT
 })();
 
 console.log(c);
+*/
+/*
+//////////////////////////
+// Strings
 
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+ 
+function calcAge(year) {
+    return 2020 - year;
+}
 
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.'); 
+
+//ES6   known as Template Literals
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J'));  // startsWith  can test is strings starts with something
+console.log(n.endsWith('th'));  // endsWith      or ends with something
+console.log(n.includes('oh'));
+console.log(firstName.repeat(5));
+console.log(`${firstName} `.repeat(5));
+*/
+/*
+///////////////////////
+// Arrow functions
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5   to return an array of each elements age
+var ages5 = years.map(function(el) {    // map loops through all elements
+    return 2020 - el;
+});
+console.log(ages5);
+
+// ES6 
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+// if more than one argument is called then you need to wrap in parenthesis
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el}.`); 
+console.log(ages6);
+
+// if more than 1 line, then you need curly braces and the return is not implicit and needs to be called
+ages6 = years.map((el, index) => {
+    const now = new 
+    Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
+*/
+/*
+///////////////////////////
+// Arrow functions 2
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        var self = this;  // work around as 'this' points to the global object and returns undefined. defining it here 
+        document.querySelector('.green').addEventListener('click', 
+        function() {
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+//box5.clickMe();
+
+// ES6
+
+var box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() {
+
+        document.querySelector('.green').addEventListener('click', 
+        () => {  // arrow functions share the 'this' keyword around them
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+//box6.clickMe();
+////////////////////////////
+// examples in diffences for 'this' keyword
+function Person(name) {
+    this.name = name;
+}
+
+// ES5
+Person.prototype.myFriends5 = 
+function(friends) {
+
+    var arr = friends.map(function(el) {
+        return this.name + ' is friends with ' + el;
+    }.bind(this));  // bind workaround to have 'this' work locally 
+    console.log(arr);
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+new Person('John').myFriends5(friends);
+
+// ES6
+Person.prototype.myFriends6 = 
+function(friends) {
+
+    var arr = friends.map(el =>
+    `${this.name} is friends with ${el}`);
+
+    console.log(arr);
+}
+new
+Person('Mike').myFriends6(friends);
+*/
+
+///////////////////////////////////
+// Destructuring
 
